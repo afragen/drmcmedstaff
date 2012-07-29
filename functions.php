e7a945d71721546a81b4902ea852454cf911a650
Andy Fragen <andy@thefragens.com>
2012-07-29 08:44:24 -0700

    Added code for Theme Updater plugin

--- functions.php
+++ functions.php
@@ -1,31 +1,4 @@
 <?php
 
-add_action('init', 'drmc_child_github_updater_init');
-function drmc_child_github_updater_init() {
-
-	include_once('updater.php');
-
-	//define('WP_GITHUB_FORCE_UPDATE', true);
-
-	if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-
-		$config = array(
-			'slug' => get_stylesheet_directory(),
-			'proper_folder_name' => basename( get_stylesheet_directory() ),
-			'api_url' => 'https://api.github.com/repos/afragen/drmcmedstaff',
-			'raw_url' => 'https://raw.github.com/afragen/drmcmedstaff/master',
-			'github_url' => 'https://github.com/afragen/drmcmedstaff',
-			'zip_url' => 'https://github.com/afragen/drmcmedstaff/zipball/master',
-			'sslverify' => true,
-			'requires' => '3.0',
-			'tested' => '3.3',
-			'readme' => 'readme.txt'
-		);
-
-		new WPGitHubUpdater($config);
-
-	}
-
-}
 
 ?>
