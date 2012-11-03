b3f4a51719a2b76f3364f1dbb549ae360cbb7883
Andy Fragen <andy@thefragens.com>
2012-11-02 19:37:22 -0700

    add message to register

--- functions.php
+++ functions.php
@@ -1,3 +1,9 @@
 <?php
 
+add_action( 'register_form', 'drmc_username' );
+function drmc_username() {
+	$html = '<p class="message">You must use your DRMC username as your <strong>Username</strong> or your registration will be deleted.</p>';
+	echo $html;
+}
+
 ?>
