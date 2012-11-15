767e8a4e258c33c6beabe76f191dca7ca3ce9a17
Andy Fragen <andy@thefragens.com>
2012-11-15 11:50:25 -0800

    bylaw and rule change classes and tweak to registration notice

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 add_action( 'register_form', 'drmc_username' );
 function drmc_username() {
-	$html = '<p class="message">You must use your DRMC username as your <strong>Username</strong> or your registration will be deleted.</p>';
+	$html = '<p class="message">You must use your DRMC username as your <strong>Username</strong> or your registration will be denied.</p><br />';
 	echo $html;
 }
 
