9037f7c123532f2c9b361424ee6716b385b75ce5
Andy Fragen <andy@thefragens.com>
2012-11-26 17:04:31 -0800

    added elections template, minor .bns-login padding, zeroed functions.php

--- functions.php
+++ functions.php
@@ -1,20 +1,3 @@
 <?php
 
-add_action( 'register_form', 'drmc_username' );
-function drmc_username() {
-	$html = '<p class="message">You must use your DRMC username as your <strong>Username</strong> or your registration will be denied.</p><br />';
-	echo $html;
-}
-
-//http://nathany.com/redirecting-wordpress-subscribers
-function change_login_redirect($redirect_to, $request_redirect_to, $user) {
-  if (is_a($user, 'WP_User') && $user->has_cap('edit_posts') === false) {
-    return get_bloginfo('siteurl');
-  }
-  return $redirect_to;
-}
-// add filter with default priority (10), filter takes (3) parameters
-add_filter('login_redirect','change_login_redirect', 10, 3);
-
-
 ?>
