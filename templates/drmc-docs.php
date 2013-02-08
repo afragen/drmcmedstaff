3b9fbd6d5621667f873482e1f1b3288d1317f2ba
Andy Fragen <andy@thefragens.com>
2013-02-07 19:11:33 -0800

    use locate_template to pass variable to template part for listing files

--- templates/drmc-docs.php
+++ templates/drmc-docs.php
@@ -25,53 +25,8 @@ get_header(); ?>
 
 
 <!-- ******* -->
-<?php
-$default_dir = "/medstaff-docs/"; // lists files only for the directory which this script is run from
-
-function file_ext_strip($filename) {
-    return preg_replace('/\.[^.]*$/', '', $filename);
-}
-
-function file_replace_spacer($filename) {
-	return preg_replace('/[_-]+/', ' ', $filename);
-}
-
-function file_title_case($filename) {
-	return ucwords($filename);
-}
-	
-function list_directory($dir) {
-	$narray=array();
-
-	$dir_handle = @opendir($dir) or die("Unable to open $dir");
-	$i=0;
-	$skip_files = array(".", "..", ".htaccess", "index.php", "drmc-credentialing");
-	//while($file = readdir($dir_handle)) {
-	while (false !== ($file = readdir($dir_handle))) {
-		if(!in_array($file, $skip_files)) {
-			$narray[$i]=$file;
-			$i++;
-		}
-	}
-	natcasesort($narray); // case-insensitive sort
-	return $narray;
-	closedir($dir_handle); //closing the directory
-}
-
-// print out html
-echo "\t" . '<ul>' . "\r\n\t";
-$directory_array = list_directory(WP_CONTENT_DIR . $default_dir);
-for($i=0; $i<sizeof($directory_array); $i++) {
-	$fn = $directory_array[$i];
-	$fn = file_ext_strip($fn);
-	$fn = file_replace_spacer($fn);
-	$fn = file_title_case($fn);
-	echo "<li><a href=".chr(34) . content_url() . $default_dir . $directory_array[$i] .chr(34). ">" . $fn . "</a></li>\r\n\t";
-}
-echo '</ul>' . "\r\n\t";
-?>
-
-<!-- ****** -->
+<?php $default_dir = "/medstaff-docs/"; ?>// lists files only for the directory which this script is run from
+<?php locate_template( 'list-files.php' ); ?>
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
