23f2b4b55b400c67b456993b0a436faf8ae29318
Andy Fragen <andy@thefragens.com>
2014-03-28 22:12:50 -0700

    turn off mobile override, doesn't work in week view

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-add_filter('tribe_events_mobile_breakpoint','tribe_events_mobile_breakpoint_override');
+//add_filter('tribe_events_mobile_breakpoint','tribe_events_mobile_breakpoint_override');
 function tribe_events_mobile_breakpoint_override ($original) {
 	// Return the minimum width (in pixels) that a screen can be before before being switched to responsive view
 	return 650;
