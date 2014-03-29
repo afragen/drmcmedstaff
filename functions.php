f785b74c7cadaa34ffb7012b745c7dca8da3afe9
Andy Fragen <andy@thefragens.com>
2014-03-28 17:26:29 -0700

    add tribe_events_mobile_breakpoint override

--- functions.php
+++ functions.php
@@ -1,2 +1,7 @@
 <?php
 
+add_filter('tribe_events_mobile_breakpoint','tribe_events_mobile_breakpoint_override');
+function tribe_events_mobile_breakpoint_override ($original) {
+	// Return the minimum width (in pixels) that a screen can be before before being switched to responsive view
+	return 650;
+}
