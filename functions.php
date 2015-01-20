37a2e326433dc8289c80326d791879014a691059
Andy Fragen <andy@thefragens.com>
2015-01-19 19:35:44 -0800

    remove 'vers' args for caching

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ function enqueue_child_theme_styles() {
 	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
 }
+
+//remove query strings from static resources. This ensures that they are cached like other elements.
+add_filter( 'script_loader_src', 'ewp_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', 'ewp_remove_script_version', 15, 1 );
+function ewp_remove_script_version( $src ){
+	return remove_query_arg( 'ver', $src );
+}
