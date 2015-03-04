9c3ab2b5afae45ea85356f816b8c1ee7f7b7b886
Andy Fragen <andy@thefragens.com>
2015-03-04 15:02:23 -0800

    darken 'Vote' button, remove WP-Polls filters

--- functions.php
+++ functions.php
@@ -12,7 +12,3 @@ add_filter( 'style_loader_src', 'ewp_remove_script_version', 15, 1 );
 function ewp_remove_script_version( $src ){
 	return remove_query_arg( 'ver', $src );
 }
-
-// secret ballots in wp-polls
-add_filter( 'poll_log_show_log_filter', '__return_false' );
-add_filter( 'poll_log_secret_ballot', '__return_empty_string' );
