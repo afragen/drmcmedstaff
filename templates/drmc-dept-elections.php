70b8e89ece8f4c61adaef2825ce7d3c8fd051a19
Andy Fragen <andy@thefragens.com>
2016-03-31 13:24:30 -0700

    improve formatting of election titles, spacing

--- templates/drmc-dept-elections.php
+++ templates/drmc-dept-elections.php
@@ -9,10 +9,10 @@
  * any active widgets from the Main Sidebar area, and the sidebar will
  * disappear everywhere.
  *
- * @package WordPress
+ * @package    WordPress
  * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
-*/
+ * @since      Twenty Twelve 1.0
+ */
 
 namespace Fragen\DRMC;
 
@@ -25,41 +25,44 @@ $args  = array(
 			'taxonomy' => 'department',
 			'field'    => 'slug',
 			'terms'    => $terms,
-			),
+		),
 		array(
 			'taxonomy' => 'department',
 			'field'    => 'slug',
 			'terms'    => array( 'voting-over', 'medical-staff' ),
 			'operator' => 'NOT IN',
-			),
 		),
-	);
+	),
+);
 
 get_header(); ?>
 
-		<div id="primary">
-			<div id="content" role="main">
+<div id="primary">
+	<div id="content" role="main">
 
 		<article class="hentry drmc_voting">
 			<div class="entry-class wrap clear">
-			<header class="entry-header">
-				<h1 class="entry-title">Items for voting - <?php echo ucwords( $terms[0] ); ?></h1>
-			</header>
-			<div class="entry-content">
-			<p>Once cast, <strong>your vote cannot be changed</strong>.</p>
+				<header class="entry-header">
+					<h1 class="entry-title">Items for voting - <?php echo ucwords( $terms[0] ); ?></h1>
+				</header>
+				<div class="entry-content">
+					<p>Once cast, <strong>your vote cannot be changed</strong>.</p>
 
-			<p>Changes and such will have the following styling. Additions will be <span class="des-insert">blue and underlined</span>. Deletions will be <span class="des-delete">red and strike-through</span>.</p>
-			</div>
+					<p>Changes and such will have the following styling. Additions will be
+						<span class="des-insert">blue and underlined</span>. Deletions will be
+						<span class="des-delete">red and strike-through</span>.</p>
+				</div>
 				<?php $my_query = new \WP_Query( $args ); ?>
 				<?php while ( $my_query->have_posts() ) : $my_query->the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-				<?php
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+					<?php
 					global $withcomments;
 					$withcomments = true;
 					comments_template();
+					?>
+				<?php endwhile; // end of the loop.
 				?>
-				<?php endwhile; // end of the loop. ?>
 
 				<?php if ( empty( $my_query->posts ) ): ?>
 					<?php get_template_part( 'template-parts/content', 'page' ); ?>
@@ -67,9 +70,9 @@ get_header(); ?>
 				<?php endif; ?>
 
 			</div><!-- .entry-class -->
-			</article>
+		</article>
 
-			</div><!-- #content -->
-		</div><!-- #primary -->
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_footer(); ?>
